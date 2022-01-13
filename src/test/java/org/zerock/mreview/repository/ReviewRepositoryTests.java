ec409b727e7b8c411c99878da3924d707528b60d
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-13 11:23:27 +0900

    ADD: 특정영화의 리뷰 조회하는 쿼리 테스트 #2

--- src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -7,6 +7,7 @@ import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -40,4 +41,26 @@ public class ReviewRepositoryTests {
         });
     }
 
+    // Review 클래스의 Member가 LAZY 방식이기 때문에 한번에 조회 할 수 없다.
+    // 기존과 달리 Transactional을 적용하여도 내부 .getMember().getEmail() 처리시 Member객체를 로딩해야한다.
+    // 해결방법
+    // 1. @Query를 이용하여 조인 처리를 하거나
+    // 2. @EntityGraph를 이용해서 Review 객체를 가져올 때 Member객체를 로딩한다.
+    @Test
+    public void testGetMovieReviews(){
+        Movie movie = Movie.builder()
+                .mno(92L)
+                .build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+            System.out.println(movieReview.getReviewnum());
+            System.out.println("\t"+ movieReview.getGrade());
+            System.out.println("\t"+ movieReview.getText());
+            System.out.println("\t"+ movieReview.getMember().getEmail());
+            System.out.println("-----------------------");
+
+        });
+    }
 }
