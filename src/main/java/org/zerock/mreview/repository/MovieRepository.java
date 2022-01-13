419b21d9630854a7ac9eec1169c4b00086b9af30
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-13 11:14:43 +0900

    ADD: 특정 영화, 영화 이미지 정보 가져오는 쿼리 작성 #2

--- src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.zerock.mreview.entity.Movie;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    // 페이지 처리
     // max() 사용대신 mi를 출력하여 가장 낮은 번호를 자동으로 연결할 수 있다.
     @Query("select m, mi, avg(coalesce(r.grade,0)), count(distinct r) " +
             "from Movie m " +
@@ -22,4 +25,10 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     and i.inum = (select max(i2.inum) from MovieImage i2 where i2.movie = m)
     left outer join Review r on r.movie = m group by m
      */
+
+    // 특정 영화 조회
+    @Query("select m, mi " +
+            "from Movie m left outer join MovieImage mi on mi.movie = m " +
+            "where m.mno = :mno")
+    List<Object[]> getMovieWithAll(Long mno);
 }
