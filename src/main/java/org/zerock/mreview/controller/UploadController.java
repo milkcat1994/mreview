97f6e7f7b860cf493f8f06bcfcd495534d9f7b48
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-13 13:58:22 +0900

    ADD: 업로드 결과 이미지로 화면에 출력 #6

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,8 +2,11 @@ package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +14,8 @@ import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -70,6 +75,33 @@ public class UploadController {
             }
         }
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    // /display?fileName=xxxx URL로 호출시에 이미지가 전송 되도록 메서드를 추가한다.
+    // URL 인코딩된 파일 이름을 파라미터로 받아 해당 파일을 byte[]로 만들어서 브라우저로 전송
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
+        ResponseEntity<byte[]> result = null;
+
+        try{
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            log.info("fileName: "+ srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: "+ file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        }
+        catch (Exception e){
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 
     private String makeFolder(){
