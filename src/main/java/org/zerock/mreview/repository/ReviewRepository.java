1b5190c26a14e1f2321b197361cb50f85ba67810
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-13 11:29:43 +0900

    FIX: EntityGraph를 이용한 EAGER 로딩 적용 #2

--- src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++ src/main/java/org/zerock/mreview/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package org.zerock.mreview.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
@@ -8,5 +9,12 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    /*
+    attributePaths : 로딩 설정을 변경하고 싶은 속성의 이름을 배열로 명시
+    type : @EntityGraph를 어떤 방식으로 적용할지 설정
+    FETCH 속성값은 attributePaths에 명시한 속성은 EAGER로, 나머지는 LAZY로 처리
+    LOAD 속성값은 attributePaths에 명시한 속성은 EAGER로, 나머지는 엔티티 클래스에 명시 or 기본 방식 처리
+     */
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
 }
