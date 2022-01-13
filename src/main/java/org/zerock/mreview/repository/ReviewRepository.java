1f672b6d62ff1b45a8ac3155510b1d3f53b9bfa6
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-13 11:38:42 +0900

    FIX: Member 삭제 쿼리 코드 수정 #2 - Query를 이용하여 Where절 사용

--- src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++ src/main/java/org/zerock/mreview/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package org.zerock.mreview.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
@@ -19,5 +21,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
 
+    // Update, Delete 이용하기 위해서는 @Modifying 어노테이션 필수
+    @Modifying
+    @Query("delete from Review mr where mr.member = :member")
     void deleteByMember(Member member);
 }
