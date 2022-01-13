a94dbb4e25c1a3da82a77c9ee69c261b207a585d
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-13 13:49:28 +0900

    ADD: 업로드 결과 반환 #6

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,9 +2,12 @@ package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +15,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,16 +27,17 @@ public class UploadController {
     private String uploadPath;
 
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for(MultipartFile uploadFile : uploadFiles){
 
             // 이미지 파일만 업로드 가능
             if(uploadFile.getContentType().startsWith("image") == false){
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
-
 
             try{
                 // 실제 파일 이름 IE나 Edge는 전체 경로가 들어온다.
@@ -52,6 +58,7 @@ public class UploadController {
 
                 try{
                     uploadFile.transferTo(savePath);
+                    resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
                 }
                 catch (IOException ioe){
                     ioe.printStackTrace();
@@ -59,9 +66,10 @@ public class UploadController {
             }
             catch (NullPointerException npe){
                 npe.printStackTrace();
-                return;
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
         }
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder(){
